cd877731899f09c1818caf2d5ab584a7422282f1
ramil290989 <garifullinramil290989@gmail.com>
2024-02-15 00:36:22 +0500

    add register page

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const RegisterPage = () => {
+  const [passwordHidden, setPasswordHidden] = useState(true);
+  const [confirmPasswordHidden, setConfirmPasswordHidden] = useState(true);
+
+  return (
+    <div className="form-card-outer">
+      <div className="form-card">
+        <h2 className="text-h2">Регистрация</h2>
+        <div className="input-box">
+          <label htmlFor="name" className="text">Имя</label>
+          <input id="name" type="text" />
+        </div>
+        <div className="input-box">
+          <label htmlFor="email" className="text">Email</label>
+          <input type="text" id="email" />
+        </div>
+        <div className="input-box">
+          <label htmlFor="password" className="text">Пароль</label>
+          <input id="password" type={passwordHidden ? 'password' : 'text'} />
+          <button
+            type="button"
+            aria-label="eyeButton"
+            className="eye-button"
+            style={{
+              backgroundImage: (
+                passwordHidden
+                  ? 'url("./eye_off.svg")'
+                  : 'url("./eye_on.svg")'
+              ),
+            }}
+            onClick={() => setPasswordHidden(!passwordHidden)}
+          />
+        </div>
+        <div className="input-box">
+          <label htmlFor="confirmPassword" className="text">Подтвердите пароль</label>
+          <input id="confirmPassword" type={confirmPasswordHidden ? 'password' : 'text'} />
+          <button
+            type="button"
+            aria-label="eyeButton"
+            className="eye-button"
+            style={{
+              backgroundImage: (
+                confirmPasswordHidden
+                  ? 'url("./eye_off.svg")'
+                  : 'url("./eye_on.svg")'
+              ),
+            }}
+            onClick={() => setConfirmPasswordHidden(!confirmPasswordHidden)}
+          />
+        </div>
+        <button type="button" className="text">Зарегистрироваться</button>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;

2cff3c4c484e9137ae8b6560f40deb75b8f3b106
ramil290989 <garifullinramil290989@gmail.com>
2024-02-20 01:24:08 +0500

    fix

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <div className="header-text-frame">
+      <h1 className="text-h1 header-text">Наша команда</h1>
+      <p className="text-h2 header-text">Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций.</p>
+    </div>
+  </div>
+);
+
+export default Header;
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -2,11 +2,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../../slices/usersSlice.js';
+import Header from '../../components/Header.jsx';
+import Users from '../../components/Users.jsx';
 
 const MainPage = () => {
   const dispatch = useDispatch();
   const loadingStatus = useSelector((state) => state.users.loadingStatus);
-  const users = useSelector((state) => state.users.userList);
 
   useEffect(() => {
     if (loadingStatus === 'loading') {
@@ -14,17 +15,31 @@ const MainPage = () => {
     }
   }, [loadingStatus]);
 
-  return (
-    <>
-      <div className="header">
-        <div className="header-text-frame">
-          <h1 className="text-h1 header-text">Наша команда</h1>
-          <p className="text-h2 header-text">Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций.</p>
-        </div>
-      </div>
-      {users.map((u) => (u.name))}
-    </>
-  );
+  switch (loadingStatus) {
+    case 'loading':
+      return (
+        <>
+          <Header />
+          загрузка
+        </>
+      );
+    case 'failed':
+      return (
+        <>
+          <Header />
+          ошибка загрузки
+        </>
+      );
+    case 'complete':
+      return (
+        <>
+          <Header />
+          <Users />
+        </>
+      );
+    default:
+      return null;
+  }
 };
 
 export default MainPage;

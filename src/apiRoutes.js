cba9c82a5b384f69261a0f26b7bad30309515cda
ramil290989 <garifullinramil290989@gmail.com>
2024-02-20 01:23:45 +0500

    add remove like

--- src/apiRoutes.js
+++ src/apiRoutes.js
@@ -3,6 +3,7 @@ const server = process.env.SERVER || 'http://localhost:3003';
 const apiRoutes = {
   login: () => `${server}/api/login`,
   fetchData: () => `${server}/api/data`,
+  like: () => `${server}/api/like`,
 };
 
 export default apiRoutes;

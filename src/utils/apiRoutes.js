9036b26e74b9d7788a6b11e9dcd575d99093c847
ramil290989 <garifullinramil290989@gmail.com>
2024-02-27 19:17:40 +0500

    add register route

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -2,6 +2,7 @@ const server = process.env.SERVER || 'http://localhost:3003';
 
 const apiRoutes = {
   login: () => `${server}/api/login`,
+  register: () => `${server}/api/register`,
   fetchData: () => `${server}/api/data`,
   like: () => `${server}/api/like`,
 };
